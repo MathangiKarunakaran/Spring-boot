e33d29581236b8162f416d87794e43c22ada100d
MathangiKarunakaran <mathangi100@gmail.com>
2019-04-11 18:01:17 +0530

    Add files via upload

---
+++ student-service/src/main/java/com/mathangi/sms/model/Address.java
@@ -0,0 +1,36 @@
+package com.mathangi.sms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto increments
+	private int id;
+	private String street;
+	private String city;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
--- student-service/src/main/java/com/mathangi/sms/model/Student.java
+++ student-service/src/main/java/com/mathangi/sms/model/Student.java
@@ -1,15 +1,47 @@
 package com.mathangi.sms.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Student {
 	@Id
 	private Integer id;
 	private String name;
-	private String city;
 	
+	@OneToOne(cascade=CascadeType.ALL)// to save
+	private Address address;
+	
+	
+	//@OneToMany(mappedBy = "student")
+	//@JoinColumn(name="id") //physical mapping
+	
+	//@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "fk_telephone")
+	@OneToMany(mappedBy="student", cascade=CascadeType.ALL)
+	List<Telephone> telephones;
+	
+	
+	public List<Telephone> getTelephones() {
+		return telephones;
+	}
+	public void setTelephones(List<Telephone> telephones) {
+		this.telephones = telephones;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -21,12 +53,6 @@ public class Student {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
 	}
 	
 	
